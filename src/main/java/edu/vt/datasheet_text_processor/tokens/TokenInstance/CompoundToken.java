92c656ccf21b04d4bbab60cc73337866c65def5b
Steven <sfrederiksen@vt.edu>
2019-11-11 17:42:18 -0500

    Give compound token a default ctor

--- src/main/java/edu/vt/datasheet_text_processor/tokens/TokenInstance/CompoundToken.java
+++ src/main/java/edu/vt/datasheet_text_processor/tokens/TokenInstance/CompoundToken.java
@@ -1,9 +1,14 @@
 package edu.vt.datasheet_text_processor.tokens.TokenInstance;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CompoundToken {
     private List<TokenInstance> originalTokens;
+
+    public CompoundToken() {
+        this.originalTokens = new ArrayList<>();
+    }
 
     public CompoundToken(List<TokenInstance> originalTokens) {
         this.originalTokens = originalTokens;
